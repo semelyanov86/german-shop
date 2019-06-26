e31885a3f136328213e56e7edbf9cfaa9eabbf38
semelyanov86 <sergey86@gmail.com>
2019-06-26 15:26:21 +0300

    first commit

---
+++ config/vtiger.php
@@ -0,0 +1,10 @@
+<?php
+
+// vTiger API constants
+return [
+    'url' => 'path/to/vtiger/webservice.php',
+    'username' => '',
+    'accesskey' => '',
+    'persistconnection' => true,
+    'max_retries' => 10
+];
